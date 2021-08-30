ac496a813ec531480bf5d56d9840484657cfc4d8
Coder-086 <hazad9999@gmail.com>
2021-08-30 15:15:42 +0530

    Authentication in Progress

--- src/main/java/com/example/Hello/model/User.java
+++ src/main/java/com/example/Hello/model/User.java
@@ -13,20 +13,20 @@ public class User {
     private String username;
     private String password;
 
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
     public String getEmail() {
         return email;
     }
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
     }
 
     public String getPassword() {
